a79490433218f1b51456894c3619b4cfc38c3dd7
Gustavo Moura <gustavorobert92@gmail.com>
2021-11-09 01:52:31 -0300

    Adição das rotas para funções transcendentais e elementares

--- api_gateway.py
+++ api_gateway.py
@@ -9,14 +9,26 @@ CORS(app)
 
 
 transcendental = {'operation':'/transcendental', 'address':'localhost', 'port':5002, 'route':'/fatorial'}
-service_registry = [transcendental]
+soma           = {'operation':'/soma', 'address':'localhost', 'port':5001, 'route':'/soma'}
+subtracao      = {'operation':'/subtracao', 'address':'localhost', 'port':5001, 'route':'/subtracao'}
+multiplicacao  = {'operation':'/multiplicacao', 'address':'localhost', 'port':5001, 'route':'/multiplicacao'}
+divisao        = {'operation':'/divisao', 'address':'localhost', 'port':5001, 'route':'/divisao'}
+service_registry = [transcendental, soma, subtracao, multiplicacao, divisao]
 
 
 @app.route('/api_gateway/<operation>')
 def api_gateway(operation):
+    print(operation)
     for service_config in service_registry:
         if service_config['operation'] == ('/'+operation):
-            parameters = { 'int_input': request.args.get('int_input')}
+            if operation == "fatorial":
+                parameters = { 'arg1': request.args.get('arg1') }
+                url =f"http://{service_config['address']}:{str(service_config['port'])}{service_config['route']}"
+                url_request = req.urlopen(f"{url}?{parse.urlencode(parameters)}")
+                result = url_request.read()
+                print(url_request)
+                return result
+            parameters = { 'arg1': request.args.get('arg1'), 'arg2': request.args.get('arg2') }
             url =f"http://{service_config['address']}:{str(service_config['port'])}{service_config['route']}"
             url_request = req.urlopen(f"{url}?{parse.urlencode(parameters)}")
             result = url_request.read()
