ff7c06f1432796481cc7b8a43b27c0435ef20a51
Gustavo Moura <gustavorobert92@gmail.com>
2021-11-09 23:32:08 -0300

    Persistence Log

--- api_gateway.py
+++ api_gateway.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, request
 from flask import jsonify
 from flask_cors import CORS
@@ -21,20 +22,26 @@ def api_gateway(operation):
     print(operation)
     for service_config in service_registry:
         if service_config['operation'] == ('/'+operation):
-            if operation == "fatorial":
+            if operation == "transcendental":
                 parameters = { 'arg1': request.args.get('arg1') }
                 url =f"http://{service_config['address']}:{str(service_config['port'])}{service_config['route']}"
                 url_request = req.urlopen(f"{url}?{parse.urlencode(parameters)}")
                 result = url_request.read()
-                print(url_request)
+                persistenceBD(operation, 'fatorial', [parameters['arg1']])
                 return result
             parameters = { 'arg1': request.args.get('arg1'), 'arg2': request.args.get('arg2') }
             url =f"http://{service_config['address']}:{str(service_config['port'])}{service_config['route']}"
             url_request = req.urlopen(f"{url}?{parse.urlencode(parameters)}")
             result = url_request.read()
-            print(url_request)
+            persistenceBD('Elementar', operation, str(parameters['arg1'])+' & '+str(parameters['arg2']))
             return result
 
+def persistenceBD(tipo_oper, operacao, args):
+    parameters = { 'tipo_oper': tipo_oper, 'operacao': operacao, 'args': args }
+    res = requests.post('http://localhost:5003/cadastrar', json=parameters)
+
+    if res.status_code == 200:
+        print('Success!')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5004)
