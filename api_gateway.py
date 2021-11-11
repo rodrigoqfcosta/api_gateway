0662bc65ed8912e0c2724b99ee351b07911315ff
Rodrigo Querino <rodrigoqfcosta@gmail.com>
2021-11-11 14:14:06 -0300

    UPDATE - Refatoração do codigo

--- api_gateway.py
+++ api_gateway.py
@@ -1,19 +1,18 @@
-import requests
 from flask import Flask, request
-from flask import jsonify
 from flask_cors import CORS
 from urllib import parse, request as req
 
+import requests
 
 app = Flask(__name__)
 CORS(app)
-
 
 transcendental = {'operation':'/transcendental', 'address':'localhost', 'port':5002, 'route':'/fatorial'}
 soma           = {'operation':'/soma', 'address':'localhost', 'port':5001, 'route':'/soma'}
 subtracao      = {'operation':'/subtracao', 'address':'localhost', 'port':5001, 'route':'/subtracao'}
 multiplicacao  = {'operation':'/multiplicacao', 'address':'localhost', 'port':5001, 'route':'/multiplicacao'}
 divisao        = {'operation':'/divisao', 'address':'localhost', 'port':5001, 'route':'/divisao'}
+
 service_registry = [transcendental, soma, subtracao, multiplicacao, divisao]
 
 
@@ -27,7 +26,7 @@ def api_gateway(operation):
                 url =f"http://{service_config['address']}:{str(service_config['port'])}{service_config['route']}"
                 url_request = req.urlopen(f"{url}?{parse.urlencode(parameters)}")
                 result = url_request.read()
-                persistenceBD(operation, 'fatorial', [parameters['arg1']])
+                persistenceBD(operation, 'fatorial', str(parameters['arg1']))
                 return result
             parameters = { 'arg1': request.args.get('arg1'), 'arg2': request.args.get('arg2') }
             url =f"http://{service_config['address']}:{str(service_config['port'])}{service_config['route']}"
